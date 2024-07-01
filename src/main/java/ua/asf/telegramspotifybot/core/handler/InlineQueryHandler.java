864eff34909d15f00f5a84557e64db08671d0cb6
r1pth3sl1t <yaroslavmolchembayev@gmail.com>
2024-07-01 23:00:36 +0300

    Refactored handlers

--- src/main/java/ua/asf/telegramspotifybot/core/handler/InlineQueryHandler.java
+++ src/main/java/ua/asf/telegramspotifybot/core/handler/InlineQueryHandler.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.AnswerInlineQuery;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.inlinequery.inputmessagecontent.InputTextMessageContent;
+import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResultArticle;
 import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResultAudio;
 import ua.asf.telegramspotifybot.core.service.SpotifyAuthorizationService;
 import ua.asf.telegramspotifybot.requests.spotify.SpotifyApiClient;
@@ -12,6 +14,7 @@ import ua.asf.telegramspotifybot.requests.spotify.entity.Track;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class InlineQueryHandler implements Handler {
@@ -24,18 +27,28 @@ public class InlineQueryHandler implements Handler {
 
     @Override
     public BotApiMethod<?> handle(Update update) {
-        if(!service.isAuthorized(update.getInlineQuery().getFrom().getId())) return null;
+
+        if(!service.isAuthorized(update.getInlineQuery().getFrom().getId())) return AnswerInlineQuery.builder()
+                .inlineQueryId(update.getInlineQuery().getId())
+                .result(InlineQueryResultArticle.builder()
+                        .id(UUID.randomUUID().toString())
+                        .title("Authorized account not found")
+                        .url("https://t.me/asf000000_bot")
+                        .inputMessageContent(new InputTextMessageContent("."))
+                        .build())
+                .cacheTime(1)
+                .isPersonal(true)
+                .build();
 
         service.refreshToken(update.getInlineQuery().getFrom().getId());
 
-        List<Track> recentlyPlayedTracks = this.spotifyApiClient.getRecentlyPlayedTracks(
+        List<Track> playedTracks =  this.spotifyApiClient.getRecentlyPlayedTracks(
                 this.service.getTokenByChatId(update.getInlineQuery().getFrom().getId())
         );
         List<InlineQueryResultAudio> result = new LinkedList<>();
-        int i = 0;
-        for (Track t : recentlyPlayedTracks) {
+        for (Track t : playedTracks) {
             result.add(InlineQueryResultAudio.builder()
-                    .id(String.valueOf(i++))
+                    .id(UUID.randomUUID().toString())
                     .title(t.getName())
                     .performer(t.getArtistsAsString())
                     .audioUrl(t.getPreviewUrl())
@@ -44,6 +57,8 @@ public class InlineQueryHandler implements Handler {
         return AnswerInlineQuery.builder()
                 .inlineQueryId(update.getInlineQuery().getId())
                 .results(result)
+                .cacheTime(1)
+                .isPersonal(true)
                 .build();
     }
 }
--- src/main/java/ua/asf/telegramspotifybot/core/handler/KeyboardHandler.java
+++ src/main/java/ua/asf/telegramspotifybot/core/handler/KeyboardHandler.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ua.asf.telegramspotifybot.core.handler.keyboard.SpotifyInlineKeyboardButtonsFactory;
 import ua.asf.telegramspotifybot.core.service.SpotifyAuthorizationService;
 
 @Component
